1fdfd721b48fff923c04bc0e26fff2038dbd47e8
wes hayutin <weshayutin@gmail.com>
2011-01-29 17:42:00 -0500

    asdf

--- workspace/westest/src/edu/waketech/whayutin/DumpServlet.java
+++ workspace/westest/src/edu/waketech/whayutin/DumpServlet.java
@@ -1,7 +1,11 @@
 package edu.waketech.whayutin;
 
 import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,7 +28,7 @@ public class DumpServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 	/**
@@ -32,7 +36,26 @@ public class DumpServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String file = request.getParameter("file");
-		response.setHeader("Content-Disposition", "attachment; filename=" + file); 
+		response.setContentType("application/jar");
+		response.setHeader("Content-Disposition", "attachment; filename=/" + file); 
+		
+		ServletContext ctx = getServletContext();
+		System.out.println(file);
+		//DataInputStream is = new DataInputStream(new FileInputStream(file));
+		InputStream is = ctx.getResourceAsStream(file);
+		int read = 0;
+		byte[] bytes = new byte[1024];
+		
+		ServletOutputStream os = response.getOutputStream();
+		while((is != null) && (read = is.read(bytes)) != -1){
+			System.out.println(read);
+			os.write(bytes,0,read);
+		}
+		os.flush();
+		os.close();
+		
+		
+		
 	}
 
 }
